6cdf25ffc4324e347d57b03be6b8d55efb3c63bc
Rafael Alexander <rafael@Rafaels-MacBook-Air.local>
2025-02-11 22:17:49 -0600

    updates

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -25,19 +25,20 @@ const Menu = () => {
   useGSAP(
     () => {
       gsap.set(".menu-link-item-holder", { y: 200 });
+      gsap.set(".menu-overlay", { opacity: 0 });
       tl.current = gsap
         .timeline({ paused: true })
         .to(".menu-overlay", {
-          duration: 1,
-          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-          ease: "power4.inOut",
+          duration: 0.4,
+          opacity: 1,
+          ease: "power4.out",
         })
         .to(".menu-link-item-holder", {
           y: 0,
-          duration: 0.75,
+          duration: 0.5,
           stagger: 0.1,
-          ease: "power4.out",
-          delay: -0.75,
+          ease: "power3.inOut",
+          delay: -0.1,
         });
     },
     { scope: container }
@@ -56,16 +57,16 @@ const Menu = () => {
       <div className="fixed bottom-0 left-0 w-screen p-2 z-[1]">
         <div className="w-full max-w-[var(--max-width)] mx-auto flex justify-between items-center">
           <div>
-            <a href="/" className="hover:underline">Linkedin</a>,
-            <a href="/" className="hover:underline">Twitter</a>
+            <a href="/" className="hover:underline p-1">Linkedin</a>,
+            <a href="/" className="hover:underline p-1">Twitter</a>
           </div>
-          <button onClick={toggleMenu} className="pointer hover:underline">
+          <button onClick={toggleMenu} className="pointer hover:underline py-1 px-2 rounded-full hover:bg-[var(--foreground-transparent)]">
             Menu
           </button>
         </div>
       </div>
 
-      <div className="menu-overlay fixed top-0 left-0 w-[100vw] h-[100dvh] p-2 bg-[var(--background-transparent)] backdrop-blur-lg z-[2] flex clip-path-menu">
+      <div className="menu-overlay fixed top-0 left-0 w-[100vw] h-[100dvh] p-2 bg-[var(--background-transparent)] backdrop-blur-lg z-[2] flex">
         <div className="w-full max-w-[var(--max-width)] mx-auto flex flex-col justify-between">
           <div></div>
           <div className="flex flex-col">
@@ -90,7 +91,7 @@ const Menu = () => {
             <div>
 
             </div>
-            <button onClick={toggleMenu} className="pointer hover:underline">
+            <button onClick={toggleMenu} className="pointer hover:underline py-1 px-2 rounded-full hover:bg-[var(--foreground-transparent)]">
               Cerrar
             </button>
           </div>
